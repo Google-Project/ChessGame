841e592ce1658c8867f35138919f9bd1c0129f1c
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2023-01-10 18:22:49 -0500

    adjusted timer

--- board.js
+++ board.js
@@ -5,6 +5,15 @@ function initializeBoard(){
     //...
 }
 
+//TIMER
+var sec = 0;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+setInterval( function(){
+    document.getElementById("seconds").innerHTML=pad(++sec%60);
+    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+}, 1000);
+
+
 var cells = document.querySelectorAll('td');
 cells.forEach(element=>{
     element.piece = Pawn()
@@ -17,9 +26,3 @@ cells.forEach(element=>{
     }
 })
 
-var sec = 0;
-function pad ( val ) { return val > 9 ? val : "0" + val; }
-setInterval( function(){
-    document.getElementById("seconds").innerHTML=pad(++sec%60);
-    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-}, 1000);
